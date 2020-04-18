61831fe1794e33feab8634c2dd6b1cdaee127725
Docta Rhyf <drrhyf@gmail.com>
2020-04-18 14:13:59 +0200

    Initial commit

---
+++ app/src/main/java/com/doctarhyf/collapsible_toolbar_sample/MainActivity.java
@@ -0,0 +1,73 @@
+package com.doctarhyf.collapsible_toolbar_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.palette.graphics.Palette;
+
+import android.app.ActionBar;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView toolbar_image;
+    private CoordinatorLayout coordinatorLayout;
+    private AppBarLayout appBarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final Display dWidth = getWindowManager().getDefaultDisplay();
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.cordinator_layout);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbar_layout);
+        appBarLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                int heightPx = dWidth.getWidth() * 1 / 3;
+                setAppBarOffset(heightPx);
+            }
+        });
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+        ImageView toolbarImage = (ImageView) findViewById(R.id.toolbar_image);
+        toolbarImage.getLayoutParams().height = dWidth.getWidth();
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.icon_user_profile_pic);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                int mutedColor = palette.getMutedColor(getResources().getColor(R.color.colorPrimary));
+                collapsingToolbarLayout.setContentScrimColor(mutedColor);
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void setAppBarOffset(int offsetPx) {
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appBarLayout.getLayoutParams();
+        AppBarLayout.Behavior behavior = (AppBarLayout.Behavior) params.getBehavior();
+        behavior.onNestedPreScroll(coordinatorLayout, appBarLayout, null, 0, offsetPx, new int[]{0, 0});
+    }
+}
